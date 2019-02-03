d3e4135cb906c96637bd350142b7145c13ed49d5
Islam ezzat <islam3zzat@gmail.com>
2019-02-03 16:50:13 +0100

    build

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Islam Farg 21
+            Islam Farg
           </a>
         </header>
       </div>
